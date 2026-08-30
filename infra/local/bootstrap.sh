79347247925a8230ca7dedd787bf4e90ec809fff
Pedro Augusto Pereira <pereira.pedro23@edu.pucrs.br>
2026-08-30 20:13:25 -0300

    chore: bootstrap cloudformation

---
+++ infra/local/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+echo "Applying Alibe local infrastructure..."
+
+aws \
+  --endpoint-url=http://ministack:4566 \
+  cloudformation deploy \
+  --template-file /infra/cloudformation/alibe.yml \
+  --stack-name alibe-local \
+  --parameter-overrides \
+    MediaBucketName="${AWS_S3_BUCKET}" \
+  --region "${AWS_REGION}" \
+  --no-fail-on-empty-changeset
+
+echo "Alibe local infrastructure ready."
